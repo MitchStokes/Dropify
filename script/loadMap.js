f1f236b6ec173637d432031f01922bb34b69d811
Mitchel Stokes <mstokes1671@gmail.com>
2022-05-17 15:24:55 -0700

    Removed troubleshooting statement

--- script/loadMap.js
+++ script/loadMap.js
@@ -5,5 +5,4 @@ const loadMap = ( isErangel ) => {
     context.drawImage( img, 0, 0, 900, 900 );
   }
   img.src = ( isErangel ) ? "res/Erangel_PNG.png" : "res/Miramar_PNG.png";
-  console.log( "test" );
 }
